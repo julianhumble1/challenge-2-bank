c69fb4d141e3f83cbe31e3c2498fc237531559ae
Julian Humble <humblejulian@gmail.com>
2024-05-06 10:25:28 +0100

    Passed test 4 of user story 5

--- spec/TransactionCreator.spec.js
+++ spec/TransactionCreator.spec.js
@@ -1,8 +1,10 @@
 import TransactionCreator from "../src/TransactionCreator.js";
 import Credit from "../src/Credit.js"
+import Debit from "../src/Debit.js"
 
 describe("TransactionCreator class tests: ", () => {
     let testCredit;
+    let testDebit;
     let testDate = new Date(2000, 0, 0);
     let testAmount = 10;
     let testResultingBalance = 10;
@@ -21,5 +23,14 @@ describe("TransactionCreator class tests: ", () => {
 
         });
 
-    })
-})
+    });
+    describe("User Story 5: ", () => {
+        it("should create an instance of type Debit when createDebit is called", () => {
+            // Assert
+            // Act
+            testDebit = TransactionCreator.createDebit(testDate, testAmount, testResultingBalance);
+            // Arrange
+            expect(testDebit).toBeInstanceOf(Debit);
+        })
+    });
+});
--- src/TransactionCreator.js
+++ src/TransactionCreator.js
@@ -1,8 +1,13 @@
 import Credit from "./Credit.js";
+import Debit from "./Debit.js"
 
 export default class TransactionCreator {
     
     static createCredit(date, amount, resultingBalance) {
         return new Credit(date, amount, resultingBalance);
+    };
+
+    static createDebit(date, amount, resultingBalance) {
+        return new Debit(date, amount, resultingBalance);
     }
 }
