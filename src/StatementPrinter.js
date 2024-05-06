06e1687decec1d79fc2f82246c0c0fe449d05816
Julian Humble <humblejulian@gmail.com>
2024-05-06 13:12:32 +0100

    fixed spelling error

--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -1,6 +1,6 @@
 export default class StatementPrinter {
-    static printStatement(Account) {
-        const transactionList = Account.getTransactionList();
+    static printStatement(account) {
+        const transactionList = account.getTransactionList();
         const headings = "date       || credit  || debit  || balance"
         let statement = headings;
         for (let i = transactionList.length - 1; i >= 0; i--) {
